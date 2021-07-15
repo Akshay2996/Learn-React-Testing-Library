3b2144eced3fabc3d6896ee92cb24ed3446f4b5c
Akshay Sharma <akshaysharmaay@gmail.com>
2021-07-15 14:47:50 +0530

    Assesrtions & describe block

--- src/components/TodoFooter/__test__/TodoFooter.test.js
+++ src/components/TodoFooter/__test__/TodoFooter.test.js
@@ -1,6 +1,35 @@
-import { render, screen } from '@testing-library/react';
-import TodoFooter from './TodoFooter';
+import { render, screen } from "@testing-library/react";
+import TodoFooter from "../TodoFooter";
+import { BrowserRouter } from "react-router-dom";
 
-it("Todo Footer Component", () => {
-    render(<TodoFooter numberOfIncompleteTasks="0" />);
-})
+const MockToDoFooter = ({ numberOfIncompleteTasks }) => (
+  <BrowserRouter>
+    <TodoFooter numberOfIncompleteTasks={numberOfIncompleteTasks} />
+  </BrowserRouter>
+);
+
+describe("ToDoFooter Component", () => {
+  it("should render correct amount of incomplete task", () => {
+    render(<MockToDoFooter numberOfIncompleteTasks={5} />);
+    const paragraphElement = screen.getByText(/5 tasks left/i);
+    expect(paragraphElement).toBeInTheDocument();
+  });
+
+  it("should render task when no. is 1", () => {
+    render(<MockToDoFooter numberOfIncompleteTasks={1} />);
+    const paragraphElement = screen.getByText(/1 task left/i);
+    expect(paragraphElement).toBeInTheDocument();
+  });
+
+  it("should render task when no. is 1 by testID", () => {
+    render(<MockToDoFooter numberOfIncompleteTasks={1} />);
+    const paragraphElement = screen.getByTestId("para");
+    expect(paragraphElement).toHaveTextContent(/^1 task left$/i);
+  });
+
+  it("should render task testID with attributes", () => {
+    render(<MockToDoFooter numberOfIncompleteTasks={1} />);
+    const paragraphElement = screen.getByTestId("para");
+    expect(paragraphElement.textContent).toBe("1 task left");
+  });
+});
