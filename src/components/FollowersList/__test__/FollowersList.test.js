e431f77e24b2b663ef9df08b537d3fbef9cec754
Akshay Sharma <akshaysharmaay@gmail.com>
2021-07-20 18:00:46 +0530

    Testing API calls

---
+++ src/components/FollowersList/__test__/FollowersList.test.js
@@ -0,0 +1,23 @@
+import { render, screen } from "@testing-library/react";
+import { BrowserRouter } from "react-router-dom";
+import FollowersList from "../FollowersList";
+
+const MockFollowersList = () => (
+  <BrowserRouter>
+    <FollowersList />
+  </BrowserRouter>
+);
+
+describe("FollowersList", () => {
+  it("Should render follower item", async () => {
+    render(<MockFollowersList />);
+    const followerDivElement = await screen.findByTestId("follower-item-0");
+    expect(followerDivElement).toBeInTheDocument();
+  });
+
+  it("Should render multiple follower items", async () => {
+    render(<MockFollowersList />);
+    const followerDivElements = await screen.findAllByTestId(/follower-item/i);
+    expect(followerDivElements.length).toBe(5);
+  });
+});
